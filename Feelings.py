f2a1cf8c8d6f99836a8a467a03f075b40a165af7
PegasusAi <82119277+thePegasusai@users.noreply.github.com>
2023-01-17 17:40:49 -0800

    Create Feelings.py

---
+++ Feelings.py
@@ -0,0 +1,8 @@
+import random
+
+feelings = ["happy", "content", "joyful", "excited", "relieved"]
+
+def make_me_feel_good():
+    print("I'm feeling", random.choice(feelings))
+
+make_me_feel_good()
